23169724bc414406f52b6bc42e3518d89feb7622
Michael322312 <zomisha0808@gmail.com>
2024-02-13 12:20:56 +0200

    Created model Post and added to list our django-app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 63)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now = True)
+
+    photo = models.FileField(upload_to="static/photos/")
+
+
