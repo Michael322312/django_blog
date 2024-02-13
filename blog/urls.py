dfa0d32295faf60870f993e993d900cf5df0ce7a
Michael322312 <zomisha0808@gmail.com>
2024-02-13 12:55:06 +0200

    Created view and addet html paths

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+import blog.views as blog_views
+
+urlpatterns = {
+    path("", blog_views.posts_list, name="posts_list")
+}
--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from blog.models import Post
 # Create your views here.
 
-def post_list(request):
+def posts_list(request):
     posts = Post.objects.all()
     context = {
         "posts_list": posts
     }
     return render(
         template_name="blog/posts_list.html",
-        context=context
+        context=context,
+        request=request
     )
