6ee23e5d818b7f6ce3234ba47feae4cebd4ef7ec
Michael322312 <zomisha0808@gmail.com>
2024-02-13 16:26:18 +0200

    Added images to posts

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 63)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now = True)
-
+    photo = models.ImageField(default=None)
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,4 @@ import blog.views as blog_views
 urlpatterns = [
     path("", blog_views.posts_list, name="posts_list"),
     path("<int:post_id>/", blog_views.view_post, name="post_details")
-]
+] 
