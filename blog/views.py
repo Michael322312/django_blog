500fd001eb1c0d7d2567930b89077f0e2eea8c3a
Michael322312 <zomisha0808@gmail.com>
2024-02-13 12:46:40 +0200

    Created in view and added to setting image path

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        "posts_list": posts
+    }
+    return render(
+        template_name="blog/posts_list.html",
+        context=context
+    )
